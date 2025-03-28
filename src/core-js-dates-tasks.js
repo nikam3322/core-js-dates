ed2a391b5e6dabdc5b712db634d83e6c5e8495cf
nikam3322 <nikam3322@gmail.com>
2025-03-28 16:30:57 +0100

    feat: update solution

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -116,8 +116,10 @@ function getCountDaysInMonth(month, year) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  const dateStartObj = new Date(dateStart);
+  const dateEndObj = new Date(dateEnd);
+  return (dateEndObj.getTime() - dateStartObj.getTime()) / 1000 / 3600 / 24 + 1;
 }
 
 /**
@@ -137,8 +139,17 @@ function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
  * '2024-02-02', { start: '2024-02-02', end: '2024-03-02' } => true
  * '2024-02-10', { start: '2024-02-02', end: '2024-03-02' } => true
  */
-function isDateInPeriod(/* date, period */) {
-  throw new Error('Not implemented');
+function isDateInPeriod(date, period) {
+  const dateObj = new Date(date);
+  const dateStartObj = new Date(period.start);
+  const dateEndObj = new Date(period.end);
+  if (
+    dateObj.getTime() >= dateStartObj.getTime() &&
+    dateObj.getTime() <= dateEndObj.getTime()
+  ) {
+    return true;
+  }
+  return false;
 }
 
 /**
@@ -152,8 +163,21 @@ function isDateInPeriod(/* date, period */) {
  * '1999-01-05T02:20:00.000Z' => '1/5/1999, 2:20:00 AM'
  * '2010-12-15T22:59:00.000Z' => '12/15/2010, 10:59:00 PM'
  */
-function formatDate(/* date */) {
-  throw new Error('Not implemented');
+function formatDate(date) {
+  const options = {
+    year: 'numeric',
+    month: 'numeric',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+    timeZone: 'UTC',
+  };
+  const dateObj = new Date(date);
+  const result = new Intl.DateTimeFormat('en-US', options).format(
+    dateObj.getTime()
+  );
+  return result;
 }
 
 /**
